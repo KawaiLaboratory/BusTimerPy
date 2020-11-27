5f3b489327780bee3a136d1eb2ca6ac930e83802
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-11-27 11:54:07 +0900

    renew

--- BusTimer.py
+++ BusTimer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[7]:
+
+
 import pandas as pd
 import datetime as dt
 import tkinter as tk
 import threading
 import enum
-import numpy
 
 class Application(tk.Frame):
     def __init__(self, master = None):
@@ -18,13 +20,13 @@ class Application(tk.Frame):
         master.title(u"BusTimer")
         master.geometry("1280x1030")
         master.protocol("WM_DELETE_WINDOW", self.closed(master))
-        
+
         self.today = dt.datetime.today()
         self.timetable = TimeTable(self.today)
         self.status = 0
         self.disp_go_msg = 0
         self.disp_come_msg = 0
-        
+
         self.f1 = tk.Frame(master, width=1280, height=165, bg=Color.GRAY2.value)
         self.f1.pack()
         self.f1.pack_propagate(0)
@@ -32,7 +34,7 @@ class Application(tk.Frame):
         self.l11.pack(fill=tk.Y, expand=True, anchor=tk.NW, side="left")
         self.l12 = tk.Label(self.f1, text="", font=("GN-キルゴUかなO", 88), fg=Color.ORANGE.value, bg=Color.GRAY2.value)
         self.l12.pack(fill=tk.BOTH, expand=True, anchor=tk.NW, side="left")
-        
+
         self.f2 = tk.Frame(master, width=1280, height=412)
         self.f2.pack()
         self.f2.pack_propagate(0)
@@ -54,7 +56,7 @@ class Application(tk.Frame):
         self.f4.pack_propagate(0)
         self.l4 = tk.Label(self.f4, text="00:00", fg=Color.CYAN.value)
         self.l4.pack(fill=tk.BOTH, expand=True)
-        
+
         self.f5 = tk.Frame(master, width=1280, height=82)
         self.f5.pack()
         self.f5.pack_propagate(0)
@@ -66,21 +68,21 @@ class Application(tk.Frame):
         self.b3.pack(fill=tk.BOTH, expand=True, side="left")
         self.b4 = tk.Button(self.f5, text="春夏土日", font=("GN-キルゴUかなO",20), command=self.btn_changed(3))
         self.b4.pack(fill=tk.BOTH, expand=True, side="left")
-        
+
         self.start_timer()
-        
+
     def closed(self, master):
         def inner():
             self.timer.cancel()
             master.destroy()
-        
+
     def btn_changed(self, num):
         def inner():
             self.status = num
             self.update_disp()
         return inner
-    
-    def update_disp(self):        
+
+    def update_disp(self):
         tables_bool = self.timetable.tables_dt[self.status] > self.today
         tables_bool = tables_bool.values[:, self.timetable.INDEX74]
         tables_bool_come = tables_bool[:, 0]
@@ -97,7 +99,7 @@ class Application(tk.Frame):
         tables_74dt_go   = tables_74dt_go[tables_bool_go]
         come_msg = "まもなく到着します"
         go_msg = "まもなく出発します"
-        
+
         if(len(tables_74_come) > 0):
             self.l4["text"] = tables_74_come[0]
             if(self.today+dt.timedelta(minutes=3)>pd.to_datetime(tables_74_come[0])):
@@ -111,7 +113,7 @@ class Application(tk.Frame):
         else:
             self.l4["text"] = "--:--"
             self.l32["text"] = ""
-            
+
         if(len(tables_74_go) == 0):
             self.l21["text"] = "サヨナラ！"
             self.l22["text"] = "アバーッ！"
@@ -132,35 +134,35 @@ class Application(tk.Frame):
             else:
                 self.l21["text"] = tables_74_go[0]
                 self.l22["text"] = tables_74_go[1]
-        
+
     def start_timer(self):
         self.today = dt.datetime.today()
         self.timer = threading.Timer(0.5, self.start_timer)
         self.update_disp()
         self.timer.start()
-        
+
     def __del__(self):
         self.timer.cancel()
-        
-    
+
 class TimeTable():
     def __init__(self, today):
         self.URL     = 'https://www.kanazawa-it.ac.jp/about_kit/yatsukaho.html'
         self.INDEX74 = [1, 6]
-        
-        self.tables     = pd.read_html(self.URL, match='運休', na_values="-", header=2)
+        self.tables = []
 
-        if(len(self.tables) == 4):
-            for i in range(len(self.tables)):
-                self.tables[i].to_csv("csv/{}.csv".format(i))
-        else:
+        try:
+            self.tables = pd.read_html(self.URL, match='運休', na_values="-", header=2)
+        except Exception as e:
             for i in range(4):
-                self.tables[i] = pd.read_csv("csv/{}.csv".format(i), index_col=0)
+                self.tables.append(pd.read_csv("csv/{}.csv".format(i), index_col=0))
+        else:
+            if(len(self.tables) == 4):
+                for i in range(len(self.tables)):
+                    self.tables[i].to_csv("csv/{}.csv".format(i))
 
         self.tables_str = [today.strftime('%Y/%m/%d')+"-"+table for table in self.tables]
         self.tables_dt  = [table.astype("datetime64") for table in self.tables_str]
-        
-        
+
 class Color(enum.Enum):
     ORANGE = "#ffb400"
     BLACK = "#212121"
@@ -174,4 +176,10 @@ root = tk.Tk()
 app = Application(root)
 
 app.mainloop()
+
+
+# In[ ]:
+
+
+
 
